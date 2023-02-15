921789c83b973cd691cfa19c1536e0b53cbeabb0
Felix Draxler <felix.draxler@iwr.uni-heidelberg.de>
2023-02-15 16:47:09 +0100

    Add progress bar that shows the global training progress in terms of numbers of epochs

--- trainable/utils.py
+++ trainable/utils.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 
 import warnings
 
+from pytorch_lightning.callbacks import ProgressBarBase
+from pytorch_lightning.callbacks.progress.tqdm_progress import Tqdm
+
 
 def get_activation(activation):
     """ Return the corresponding torch Activation function by string """
@@ -45,3 +48,25 @@ def make_dense(widths: list[int], activation: str, dropout: float = None):
     network.add_module("Output_Layer", output_layer)
 
     return network
+
+
+class EpochProgressBar(ProgressBarBase):
+    def __init__(self):
+        super().__init__()
+        self.bar = None
+
+    def on_train_start(self, trainer, pl_module):
+        self.bar = Tqdm(
+            desc='Epoch',
+            leave=False,
+            dynamic_ncols=True,
+            total=trainer.max_epochs,
+        )
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        self.bar.update(1)
+        self.bar.set_description(f"Epoch {trainer.current_epoch}")
+        self.bar.set_postfix(self.get_metrics(trainer, pl_module))
+
+    def on_train_end(self, trainer, pl_module):
+        self.bar.close()
