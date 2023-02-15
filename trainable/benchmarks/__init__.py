8e0479fb04c346e39e59495fed7657dc95ad4e71
LarsKue <lars@kuehmichel.de>
2023-02-15 16:26:33 +0100

    add normal and uniform dataset to benchmarks.__init__

--- trainable/benchmarks/__init__.py
+++ trainable/benchmarks/__init__.py
@@ -3,3 +3,5 @@ from .grid import benchmark_grid
 from .hypershells import HypershellsDataset
 from .hypersphere import HypersphereMixtureDataset
 from .moons import MoonsDataset
+from .normal import NormalDataset
+from .uniform import UniformDataset
