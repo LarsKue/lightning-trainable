28d46b0a148540229da0c2af8c93d9950f5bd346
LarsKue <lars@kuehmichel.de>
2023-03-29 16:25:30 +0200

    add StepProgressBar

--- src/lightning_trainable/callbacks/epoch_progress_bar.py
+++ src/lightning_trainable/callbacks/epoch_progress_bar.py
@@ -1,9 +1,9 @@
 
-from pytorch_lightning.callbacks import ProgressBarBase
-from pytorch_lightning.callbacks.progress.tqdm_progress import Tqdm
+from lightning.pytorch.callbacks import ProgressBar
+from lightning.pytorch.callbacks.progress.tqdm_progress import Tqdm
 
 
-class EpochProgressBar(ProgressBarBase):
+class EpochProgressBar(ProgressBar):
     def __init__(self):
         super().__init__()
         self.bar = None
@@ -19,6 +19,35 @@ class EpochProgressBar(ProgressBarBase):
     def on_train_epoch_end(self, trainer, pl_module):
         self.bar.update(1)
         self.bar.set_description(f"Epoch {trainer.current_epoch}")
+        self.bar.set_postfix(self.get_metrics(trainer, pl_module))
+
+    def on_train_end(self, trainer, pl_module):
+        self.bar.close()
+
+
+class StepProgressBar(ProgressBar):
+    def __init__(self):
+        super().__init__()
+        self.bar = None
+
+    def on_train_start(self, trainer, pl_module):
+        batches = len(pl_module.train_dataloader())
+
+        if trainer.max_epochs is not None:
+            total = batches * trainer.max_epochs
+        else:
+            total = trainer.max_steps
+
+        self.bar = Tqdm(
+            desc="Step",
+            leave=True,
+            dynamic_ncols=True,
+            total=total,
+        )
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        self.bar.update(1)
+        self.bar.set_description(f"Epoch {trainer.current_epoch:04d}: Batch {batch_idx:04d}")
         self.bar.set_postfix(self.get_metrics(trainer, pl_module))
 
     def on_train_end(self, trainer, pl_module):
