b55556c870f5b232b56295746995ec474f0dfe62
LarsKue <lars@kuehmichel.de>
2023-05-12 20:45:37 +0200

    more error handling

--- src/lightning_trainable/metrics/wasserstein.py
+++ src/lightning_trainable/metrics/wasserstein.py
@@ -12,12 +12,14 @@ def wasserstein(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: int | float 
     @param epsilon: The entropic regularization parameter.
     @param steps: The number of Sinkhorn iterations.
     """
+    if x.shape[1:] != y.shape[1:]:
+        raise ValueError(f"x and y must have the same feature dimensions, but got {x.shape[1:]} and {y.shape[1:]}.")
+
     if cost is None:
         cost = x[:, None] - y[None, :]
         cost = torch.flatten(cost, start_dim=2)
         cost = torch.linalg.norm(cost, dim=-1)
-
-    if cost.shape != (x.shape[0], y.shape[0]):
+    elif cost.shape != (x.shape[0], y.shape[0]):
         raise ValueError(f"Expected cost matrix of shape {(x.shape[0], y.shape[0])}, but got {cost.shape}.")
 
     u = torch.zeros(x.shape[0], device=x.device)
