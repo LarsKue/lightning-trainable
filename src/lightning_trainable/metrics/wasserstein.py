4ff8b2c8fd2bf2d962f84d78beb344e18c94d8a4
LarsKue <lars@kuehmichel.de>
2023-05-13 00:29:46 +0200

    fix wasserstein and sinkhorn

--- src/lightning_trainable/metrics/wasserstein.py
+++ src/lightning_trainable/metrics/wasserstein.py
@@ -3,43 +3,63 @@ import torch
 from torch import Tensor
 
 
-def plan(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: int | float = 0.1, steps: int = 100) -> Tensor:
-    """
-    Compute the optimal transport plan pi between two distributions.
-    @param x: Samples from the first distribution.
-    @param y: Samples from the second distribution.
-    @param cost: Optional cost matrix. If not provided, the L2 distance is used.
-    @param epsilon: The entropic regularization parameter.
-    @param steps: The number of Sinkhorn iterations.
-    """
+def _process_cost(x: Tensor, y: Tensor, cost: Tensor = None) -> Tensor:
     if x.shape[1:] != y.shape[1:]:
-        raise ValueError(f"x and y must have the same feature dimensions, but got {x.shape[1:]} and {y.shape[1:]}.")
-
+        raise ValueError(f"Expected x and y to live in the same feature space, "
+                         f"but got {x.shape[1:]} and {y.shape[1:]}.")
     if cost is None:
         cost = x[:, None] - y[None, :]
         cost = torch.flatten(cost, start_dim=2)
         cost = torch.linalg.norm(cost, dim=-1)
     elif cost.shape != (x.shape[0], y.shape[0]):
-        raise ValueError(f"Expected cost matrix of shape {(x.shape[0], y.shape[0])}, but got {cost.shape}.")
+        raise ValueError(f"Expected cost to have shape {(x.shape[0], y.shape[0])}, "
+                         f"but got {cost.shape}.")
 
-    u = torch.zeros(x.shape[0], device=x.device)
-    v = torch.zeros(y.shape[0], device=y.device)
-
-    for step in range(steps):
-        u = epsilon * torch.logsumexp(-cost + v[None, :] / epsilon, dim=1)
-        v = epsilon * torch.logsumexp(-cost + u[:, None] / epsilon, dim=0)
-
-    pi = torch.exp(-(cost + u[:, None] + v[None, :]) / epsilon)
-
-    return pi
+    return cost
 
 
-def wasserstein(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: int | float = 0.1, steps: int = 100) -> Tensor:
+def sinkhorn(a: Tensor, b: Tensor, cost: Tensor, epsilon: float = 0.1, steps: int = 100) -> Tensor:
     """
-    Compute the Wasserstein distance between two distributions. See <cref>plan</cref> for parameter descriptions.
+    Computes the Sinkhorn optimal transport plan between two distributions.
+    @param a: Sample weights from the first distribution in shape (n,)
+    @param b: Sample weights from the second distribution in shape (m,)
+    @param cost: Cost matrix in shape (n, m).
+    @param epsilon: Entropic regularization parameter.
+    @param steps: Number of iterations.
     """
-    pi = plan(x, y, cost, epsilon, steps)
+    if cost.shape != (len(a), len(b)):
+        raise ValueError(f"Expected cost to have shape {(len(a), len(b))}, but got {cost.shape}.")
 
-    w = torch.sum(pi * cost)
+    gain = torch.exp(-cost / epsilon)
 
-    return w
+    # Initialize the dual variables.
+    u = torch.ones(len(a))
+    v = torch.ones(len(b))
+
+    # Compute the Sinkhorn iterations.
+    for _ in range(steps):
+        v = b / (torch.matmul(gain.T, u) + 1e-50)
+        u = a / (torch.matmul(gain, v) + 1e-50)
+
+    # Return the transport plan.
+    return u[:, None] * gain * v[None, :]
+
+
+def wasserstein(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: float = 0.1, steps: int = 100) -> Tensor:
+    """
+    Computes the Wasserstein distance between two distributions. See <cref>sinkhorn</cref> for more details.
+    """
+    if x.shape[1:] != y.shape[1:]:
+        raise ValueError(f"Expected x and y to live in the same feature space, "
+                         f"but got {x.shape[1:]} and {y.shape[1:]}.")
+    if cost is None:
+        cost = x[:, None] - y[None, :]
+        cost = torch.flatten(cost, start_dim=2)
+        cost = torch.linalg.norm(cost, dim=-1)
+
+    # Initialize the sample weights.
+    a = torch.ones(len(x)) / len(x)
+    b = torch.ones(len(y)) / len(y)
+
+    # Compute the transport plan and return the Wasserstein distance.
+    return torch.sum(sinkhorn(a, b, cost, epsilon, steps) * cost)
