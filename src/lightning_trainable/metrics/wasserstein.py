b1fe32229e389c51e4640aab15828b308722685b
LarsKue <lars@kuehmichel.de>
2023-05-13 00:34:49 +0200

    add sinkhorn_auto, clean up

--- src/lightning_trainable/metrics/wasserstein.py
+++ src/lightning_trainable/metrics/wasserstein.py
@@ -3,24 +3,9 @@ import torch
 from torch import Tensor
 
 
-def _process_cost(x: Tensor, y: Tensor, cost: Tensor = None) -> Tensor:
-    if x.shape[1:] != y.shape[1:]:
-        raise ValueError(f"Expected x and y to live in the same feature space, "
-                         f"but got {x.shape[1:]} and {y.shape[1:]}.")
-    if cost is None:
-        cost = x[:, None] - y[None, :]
-        cost = torch.flatten(cost, start_dim=2)
-        cost = torch.linalg.norm(cost, dim=-1)
-    elif cost.shape != (x.shape[0], y.shape[0]):
-        raise ValueError(f"Expected cost to have shape {(x.shape[0], y.shape[0])}, "
-                         f"but got {cost.shape}.")
-
-    return cost
-
-
 def sinkhorn(a: Tensor, b: Tensor, cost: Tensor, epsilon: float = 0.1, steps: int = 100) -> Tensor:
     """
-    Computes the Sinkhorn optimal transport plan between two distributions.
+    Computes the Sinkhorn optimal transport plan from sample weights of two distributions.
     @param a: Sample weights from the first distribution in shape (n,)
     @param b: Sample weights from the second distribution in shape (m,)
     @param cost: Cost matrix in shape (n, m).
@@ -45,9 +30,15 @@ def sinkhorn(a: Tensor, b: Tensor, cost: Tensor, epsilon: float = 0.1, steps: in
     return u[:, None] * gain * v[None, :]
 
 
-def wasserstein(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: float = 0.1, steps: int = 100) -> Tensor:
+def sinkhorn_auto(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: float = 0.1, steps: int = 100) -> Tensor:
     """
-    Computes the Wasserstein distance between two distributions. See <cref>sinkhorn</cref> for more details.
+    Computes the Sinkhorn optimal transport plan from samples from two distributions.
+    See also: <cref>sinkhorn</cref>
+    @param x: Samples from the first distribution in shape (n, ...).
+    @param y: Samples from the second distribution in shape (m, ...).
+    @param cost: Optional cost matrix in shape (n, m). If not provided, the Euclidean distance is used.
+    @param epsilon: Entropic regularization parameter.
+    @param steps: Number of iterations.
     """
     if x.shape[1:] != y.shape[1:]:
         raise ValueError(f"Expected x and y to live in the same feature space, "
@@ -61,5 +52,12 @@ def wasserstein(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: float = 0.1,
     a = torch.ones(len(x)) / len(x)
     b = torch.ones(len(y)) / len(y)
 
-    # Compute the transport plan and return the Wasserstein distance.
-    return torch.sum(sinkhorn(a, b, cost, epsilon, steps) * cost)
+    return sinkhorn(a, b, cost, epsilon, steps)
+
+
+def wasserstein(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: float = 0.1, steps: int = 100) -> Tensor:
+    """
+    Computes the Wasserstein distance between two distributions.
+    See also: <cref>sinkhorn_auto</cref>
+    """
+    return torch.sum(sinkhorn_auto(x, y, cost, epsilon, steps) * cost)
