e048d2cc1bef7b2e16174ec34e9b3918e98f4857
LarsKue <lars@kuehmichel.de>
2023-05-16 13:22:46 +0200

    improve warning for low bandwidth

--- src/lightning_trainable/metrics/wasserstein.py
+++ src/lightning_trainable/metrics/wasserstein.py
@@ -23,7 +23,7 @@ def sinkhorn(a: Tensor, b: Tensor, cost: Tensor, epsilon: float, steps: int = 10
 
     if gain.mean() < 1e-30:
         warnings.warn(f"Detected low bandwidth ({epsilon:.1e}) relative to cost ({cost.mean().item():.1e}). "
-                      f"You may experience numerical instabilities. Consider increasing epsilon.")
+                      f"You may experience numerical instabilities. Consider increasing epsilon or using sinkhorn_log.")
 
     # Initialize the dual variables.
     u = torch.ones(len(a), dtype=a.dtype, device=a.device)
@@ -105,5 +105,9 @@ def wasserstein(x: Tensor, y: Tensor, cost: Tensor = None, epsilon: float = 0.1,
     Computes the Wasserstein distance between two distributions.
     See also: <cref>sinkhorn_auto</cref>
     """
-    # TODO: fix for cost = None
+    if cost is None:
+        cost = x[:, None] - y[None, :]
+        cost = torch.flatten(cost, start_dim=2)
+        cost = torch.linalg.norm(cost, dim=-1)
+
     return torch.sum(sinkhorn_auto(x, y, cost, epsilon, steps) * cost)
