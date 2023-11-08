7be36f6bdb815f135115e636caebb4eed180d9f1
Lars Kuehmichel <lars.kuehmichel@stud.uni-heidelberg.de>
2023-11-08 16:22:41 +0100

    Made dict-type hparams writable Fixes #17

--- src/lightning_trainable/hparams/attribute_dict.py
+++ src/lightning_trainable/hparams/attribute_dict.py
@@ -1,18 +1,23 @@
 
 
 class AttributeDict(dict):
+    def __init__(self, **kwargs):
+        for key in kwargs.keys():
+            if type(kwargs[key]) is dict:
+                kwargs[key] = AttributeDict(**kwargs[key])
+
+        super().__init__(**kwargs)
+
     def __getattribute__(self, item):
         if item not in self:
             return super().__getattribute__(item)
 
-        item = self[item]
-        if type(item) is dict:
-            # only convert nested dicts if type is exactly dict
-            return AttributeDict(item)
-
-        return item
+        return self[item]
 
     def __setattr__(self, key, value):
+        if type(value) is dict:
+            value = AttributeDict(**value)
+
         self[key] = value
 
     def copy(self):
